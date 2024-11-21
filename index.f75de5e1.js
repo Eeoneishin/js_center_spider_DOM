c388eec23c1ffbff8984c557e441b285868ef7d7
Oleksii <streetracer1001@gmail.com>
2024-11-21 21:35:16 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const wallWidth = wall.offsetWidth;
+const wallHeight = wall.offsetHeight;
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+const leftPx = (wallWidth - spiderWidth) / 2;
+const topPx = (wallHeight - spiderHeight) / 2;
+// Застосовуємо обчислену позицію до павука
+spider.style.position = "absolute";
+spider.style.left = `${leftPx}px`;
+spider.style.top = `${topPx}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
